f38dff647ffd265cf6af85af940ab5e3047a8a07
arnoldmn <arnoldnzibuk@gmail.com>
2023-08-03 09:19:00 +0300

    generate random number

--- blockExplore/contracts/RandomNumber.sol
+++ blockExplore/contracts/RandomNumber.sol
@@ -1,20 +1,22 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.20;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract RandomNumber is VRFConsumerBaseV2 {
+contract RandomNumber is VRFConsumerBase {
     bytes32 internal keyHash;
     uint internal fee;
     uint public randomResult;
-    address public owner;
-    address vrfCoordinator = 0x8103b0a8a00be2ddc778e6e7eaa21791cd364625;
 
-    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
-        keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
-        fee = 0.1 * 10 ** 18;
-        owner = msg.sender;
+    constructor()
+        VRFConsumerBase(
+            0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D,
+            0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+        )
+    {
+        keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+        fee = 0.25 * 10 ** 18;
     }
 
     function getRandomNumber() public returns (bytes32 requestId) {
