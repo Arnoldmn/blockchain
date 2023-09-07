9535f1b2a0730e2e4366c8fdea0a812f53af11cf
arnoldmn <arnoldnzibuk@gmail.com>
2023-09-07 07:07:35 +0300

    addfunds function

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract Faucet{
-	receive() external payable {}
-	function addFunds() external payable{}
+contract FaucetContract {
+
+    address[] public funders;
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+
 }
-																																																																																			
