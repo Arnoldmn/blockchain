1a6975c14fdeb5bbe258ef397955c54bd10f2dbe
Arnoldmn <arnoldnzibuk@gmail.com>
2024-09-14 01:21:08 +0300

    Modifier

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -11,6 +11,10 @@ contract FundMe{
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAMountFunded;
 
+    address public owner;
+    constructor() {
+        owner = msg.sender;
+    }
     
     function fund() public payable {
         require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough funds");
@@ -19,7 +23,10 @@ contract FundMe{
     
     }
 
-   function withdraw() public {
+   function withdraw() public onlyOwner {
+
+        require(msg.sender == owner, "Must be the owner");
+
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAMountFunded[funder] = 0;   
@@ -34,5 +41,10 @@ contract FundMe{
 
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
+   }
+
+   modifier onlyOwner() {
+    require(msg.sender == owner, "Sender is not owner!");
+    _;
    }
 }
