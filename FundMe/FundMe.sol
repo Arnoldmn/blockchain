ed530c9f4a5e4d11bc0a457ffdc9aa6b7f5e1ae9
Arnoldmn <arnoldnzibuk@gmail.com>
2024-09-17 11:07:11 +0300

    Fallback and Receive function

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe{
 
     using PriceConverter for uint256;
@@ -44,7 +46,19 @@ contract FundMe{
    }
 
    modifier onlyOwner() {
-    require(msg.sender == i_owner, "Sender is not owner!");
+    // require(msg.sender == i_owner, "Sender is not owner!");
+    if (msg.sender != i_owner) {
+        revert NotOwner();
+    }
     _;
    }
+
+   receive() external payable {
+
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+     }
 }
