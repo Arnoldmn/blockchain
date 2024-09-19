4aeb2d64470951d2486bf77e58e5136d2b5b14d3
Arnoldmn <arnoldnzibuk@gmail.com>
2024-09-19 07:53:04 +0300

    Deploying to zksync testnet

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.24;
 
-import {PriceConverter} from "./PriceConverter.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+     function getPrice() internal view returns(uint256)  {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e18);
+
+    }
+
+    function getConversionRate(uint256 ethAmount) internal  view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
+
+    }
+}
 
 error NotOwner();
 
@@ -23,6 +41,11 @@ contract FundMe{
         funders.push(msg.sender);
         addressToAMountFunded[msg.sender] += msg.value;
     
+    }
+
+    function getVervion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
+        return priceFeed.version();
     }
 
    function withdraw() public onlyOwner {
