50a6f93f8745755c0c362c3e476cfc0b76cdc37c
arnoldmn <arnoldnzibuk@gmail.com>
2025-01-05 10:10:46 +0300

    tests

---
+++ foundrytuts/foundry-fund-me/test/FundMeTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT 
+// SPDX-License-Interface: MIT
+
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
+
