9c17d2ba326184f1bd14696f392ddb6a1e73d038
arnoldmn <arnoldnzibuk@gmail.com>
2025-01-06 08:35:11 +0300

    deployement scripts

--- foundrytuts/foundry-fund-me/script/DeployFundMe.s.sol
+++ foundrytuts/foundry-fund-me/script/DeployFundMe.s.sol
@@ -2,3 +2,13 @@
 
 pragma solidity ^0.8.24;
 
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract DeployFundMe is Script{
+    function run() external {
+        vm.startBroadcast();
+        new FundMe();
+        vm.stopBroadcast();
+    }
+}
