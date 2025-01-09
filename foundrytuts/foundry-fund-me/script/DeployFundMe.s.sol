1dbc9cf05562094ae4b7763bfec50f48b3eb2fda
arnoldmn <arnoldnzibuk@gmail.com>
2025-01-09 14:41:30 +0300

    refactoring deployment

--- foundrytuts/foundry-fund-me/script/DeployFundMe.s.sol
+++ foundrytuts/foundry-fund-me/script/DeployFundMe.s.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script{
     function run() external returns (FundMe){
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694aA7C8e6954b2F6f5bE6C3Fe3c3C7c5d8c5e6F);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
